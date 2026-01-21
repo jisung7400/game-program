44b58b724111dcda8267e5236ce20410a776cd6f
jisung7400 <01051607400w@gmail.com>
2026-01-21 21:44:21 +0900

    2026 01 21 게임 설계론

--- program/source.c
+++ program/source.c
@@ -1,70 +1,135 @@
 #include <stdio.h>
 #include <conio.h>
-#include <Windows.h>
+#include <windows.h>
 
 #define UP 72
 #define LEFT 75
 #define RIGHT 77
 #define DOWN 80
 
+int screenIndex;
+HANDLE screen[2];
 
-void position(int x, int y)
+void Initialize()
 {
+	CONSOLE_CURSOR_INFO cursor;
+
+	// 화면 버퍼를 2개 생성합니다.
+	screen[0] = CreateConsoleScreenBuffer
+	(
+		GENERIC_READ | GENERIC_WRITE,
+		0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL
+	);
+
+	screen[1] = CreateConsoleScreenBuffer
+	(
+		GENERIC_READ | GENERIC_WRITE,
+		0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL
+	);
+
+	cursor.bVisible = FALSE;
+
+	SetConsoleCursorInfo(screen[0], &cursor);
+	SetConsoleCursorInfo(screen[1], &cursor);
+}
+
+void Flip()
+{
+	SetConsoleActiveScreenBuffer(screen[screenIndex]);
+
+	screenIndex = !screenIndex;
+}
+
+void Clear()
+{
+	COORD position = { 0, 0 };
+
+	DWORD dword;
+
+	CONSOLE_SCREEN_BUFFER_INFO buffer;
+
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	GetConsoleScreenBufferInfo(console, &buffer);
+
+	int width = buffer.srWindow.Right - buffer.srWindow.Left + 1;
+	int height = buffer.srWindow.Bottom - buffer.srWindow.Top + 1;
+
+	FillConsoleOutputCharacter
+	(
+		screen[screenIndex], ' ', width * height, position, &dword
+	);
+}
+
+void Release()
+{
+	CloseHandle(screen[0]);
+	CloseHandle(screen[1]);
+}
+
+void Render(int x, int y, const char* character)
+{
+	DWORD dword;
 	COORD position = { x, y };
 
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+	SetConsoleCursorPosition(screen[screenIndex], position);
+	WriteFile(screen[screenIndex], character, strlen(character), &dword, NULL);
 }
+
 
 int main()
 {
-// position(5, 5);
-
-// printf("★");
-
 	char key = 0;
+
+	CONSOLE_SCREEN_BUFFER_INFO console;
+
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	GetConsoleScreenBufferInfo(hStdout, &console);
+
+	int width = console.srWindow.Right - console.srWindow.Left - 2;
+	int height = console.srWindow.Bottom - console.srWindow.Top;
 
 	int x = 0;
 	int y = 0;
 
-	position(x, y);
+	Initialize();
 
-	printf("★");
-	
+	Render(x, y, "☆");
+
 	while (1)
 	{
-		
+		Flip();
+
+		Clear();
 
 		key = _getch();
 
-		if (key == -32 || key == 0) 
+		if (key == -32 || key == 0)
 		{
 			key = _getch();
 		}
 
 		switch (key)
 		{
-		case UP		: y--; 
-			break;
+		case UP: if (y > 0) { y--; }
+			   break;
 
-		case LEFT   : x -= 2;
-			break;
+		case LEFT: if (x > 0) { x -= 2; }
+				 break;
 
-		case RIGHT  : x += 2;
-			break;
+		case RIGHT: if (width > x) { x += 2; }
+				  break;
 
-		case DOWN	: y++; 
-			break;
-		default		: printf("exception\n");
+		case DOWN: if (height > y) { y++; }
+				 break;
+
+		default: printf("exception\n");
 			break;
 		}
 
-		system("cls");
-
-		position(x, y);
-
-		printf("★");
+		Render(x, y, "☆");
 	}
-
 
 	return 0;
 }
